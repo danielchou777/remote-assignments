153f9e301f1cf2cec7c0c2d3062f1dc50b88c49b
danielchou777 <danielchou777@gmail.com>
2023-02-05 11:32:06 +0800

    Fix Week-4 Assignment-4

--- Week-4/Assignment-4/assignment-4.sql
+++ Week-4/Assignment-4/assignment-4.sql
@@ -1,5 +1,8 @@
 -- select all articles with author’s email.
-SELECT * FROM article WHERE authorID = (SELECT id FROM user WHERE email='abc@gmail.com');
+SELECT a.id, u.email AS 'author', a.title, a.content 
+FROM article AS a 
+INNER JOIN user AS u on a.authorID = u.id 
+WHERE u.email='abc@gmail.com';
 
 -- select articles from 7th to 12th sorted by id
 SELECT * FROM article ORDER BY id LIMIT 6 OFFSET 6;
