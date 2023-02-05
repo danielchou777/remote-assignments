9d79f1c5fb84c805b8dbd5683af5ba82090b3e03
danielchou777 <danielchou777@gmail.com>
2023-02-05 15:45:38 +0800

    Fix Week-4 Assignment-3,4

--- Week-4/Assignment-3/backup.sql
+++ Week-4/Assignment-3/backup.sql
@@ -37,7 +37,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'123@gmail.com','123'),(2,'abc@gmail.com','abc'),(5,'daniel@gmail.com','123');
+INSERT INTO `user` VALUES (1,'123@gmail.com','123'),(2,'abc@gmail.com','abc'),(3,'daniel@gmail.com','123');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
